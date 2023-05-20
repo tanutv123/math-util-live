554a230a17bfcacbcdae7e9ded8449031ebf4ded
tanutv123 <taitanvo2309@gmail.com>
2023-05-20 13:06:37 +0700

    Dua code len server

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static long getFactorial(int n) {
+        if(n<0 || n > 21) {
+            throw new IllegalArgumentException("Invalid Argement. N must be between 0 and 20");
+        }
+        if(n==0 || n==1) {
+            return 1;
+        }
+        long product = 1;
+        for(int i = 2; i <=n; i++) {
+            product *= i;
+            
+        }
+        return product;
+          
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MathUtilLive {
+    public static void main(String[] args) {
+        long result = MathUtil.getFactorial(5);
+        System.out.println("5! = 120; actual = " + result);
+    }
+}
