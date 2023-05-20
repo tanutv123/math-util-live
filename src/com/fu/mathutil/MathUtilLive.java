e0fca83c93ba8fadfea2405593f1773f07e45c5c
tanutv123 <taitanvo2309@gmail.com>
2023-05-20 13:23:53 +0700

    Up code lan 2

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -12,5 +12,6 @@ public class MathUtilLive {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual = " + result);
+        System.out.println("3! = 6; actual: " + MathUtil.getFactorial(3));
     }
 }
